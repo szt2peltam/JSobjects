89cec257f9798772e7b00742cdae32e4b4212f14
szt2peltam <pelitamas494@gmail.com>
2023-10-13 21:21:52 +0200

    3. feladat kész

--- js/script.js
+++ js/script.js
@@ -3,9 +3,9 @@
 
 let user = {firstName:"John",
             lastName:"Smith",
-            }
+            };
 console.log(user);
-user.firstName = "Paul"
+user.firstName = "Paul";
 console.log(user);
 delete user["firstName"];
 
@@ -25,3 +25,29 @@ function isEmpty(object){
 console.log(isEmpty(user));
 
 //Task 2 END
+
+
+//Task 3
+
+let fizetes = {
+    Anna : 2100,
+    Cecil : 1890,
+    Emil : 2050,
+    Gerald : 2920
+ };
+
+
+ const NamesContainer = document.querySelector(".names");
+ const PaysContainer = document.querySelector(".pays");
+ const SumContainer = document.querySelector(".sum");
+
+let fizetesKeys = Object.keys(fizetes);
+let fizetesValues = Object.values(fizetes);
+let sum = 0;
+for (let index = 0; index < fizetesKeys.length; index++) {
+    NamesContainer.insertAdjacentHTML("afterbegin", `<p>${fizetesKeys[index]} :</p>`)
+    PaysContainer.insertAdjacentHTML("afterbegin", `<p>${fizetesValues[index]}</p>`)
+    sum += fizetesValues[index]
+}
+
+SumContainer.insertAdjacentHTML("beforeend", `<span>${sum}</span>`)
