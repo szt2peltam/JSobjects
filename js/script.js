89499e5880051689ccdda6a2b1cde3f641f756f3
szt2peltam <pelitamas494@gmail.com>
2023-10-13 21:08:09 +0200

    1. feladat kész

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Task 1
+
+
+let user = {firstName:"John",
+            lastName:"Smith",
+            }
+console.log(user);
+user.firstName = "Paul"
+console.log(user);
+delete user["firstName"];
+
+console.log(user);
+
+//Task 1 END
